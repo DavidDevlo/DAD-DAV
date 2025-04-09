8aef1114add4ed611dd275fa90f5ad48f9650391
Edgar David Naira Alture <117549379+DavidDevlo@users.noreply.github.com>
2025-04-09 16:06:41 -0500

    Add files via upload
    archivo gateway

---
+++ ms-gatway-server/src/main/java/com/example/msgatwayserver/MsGatwayServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.msgatwayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGatwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatwayServerApplication.class, args);
+	}
+
+}
---
+++ ms-gatway-server/src/test/java/com/example/msgatwayserver/MsGatwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msgatwayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatwayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
